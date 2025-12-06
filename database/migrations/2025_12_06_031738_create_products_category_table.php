e61031fa95c580f2e9400a8d81fd5f4f4573d3f5
brianpr-pr <brian23perezperez@gmail.com>
2025-12-06 03:22:40 +0000

    Added products category table and changes in naming conventions

--- database/migrations/2025_12_06_031738_create_products_category_table.php
+++ database/migrations/2025_12_06_031738_create_products_category_table.php
@@ -11,11 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('product_owned', function (Blueprint $table) {
+        Schema::create('products_category', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained()->cascadeOnUpdate()->cascadeOnDelete();
             $table->foreignId('product_id')->constrained()->cascadeOnUpdate()->cascadeOnDelete();
-            
             $table->timestamps();
         });
     }
@@ -25,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('product_owned');
+        Schema::dropIfExists('products_category');
     }
 };
